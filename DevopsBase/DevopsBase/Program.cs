635575ee16a3bd1fa83c51b02ddf3f97c65eabe3
Maestro <paulomaestro@employer.com.br>
2019-06-21 14:13:16 -0300

    #ENC - Criação de projeto.

---
+++ DevopsBase/DevopsBase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DevopsBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+                Console.WriteLine("Vocês esta em Desenvolvimento!");
+
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Homologation")
+                Console.WriteLine("Vocês esta em Homologação");
+
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+                Console.WriteLine("Vocês esta em Produção");
+
+            Console.Read();
+        }
+    }
+}
